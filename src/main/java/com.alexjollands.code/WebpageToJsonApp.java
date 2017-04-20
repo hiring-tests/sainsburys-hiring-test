f9fb740c263bd2485f2512037b4bcd2ddb5ca80c
Alex Jollands <alexj.ollands@googlemail.com>
2017-04-20 22:16:39 +0100

    Creating App class, main method and executable JAR

---
+++ src/main/java/com.alexjollands.code/WebpageToJsonApp.java
@@ -0,0 +1,8 @@
+package com.alexjollands.code;
+
+public class WebpageToJsonApp{
+
+    public static void main(String[] args){
+        System.out.println("Test message and JAR executable");
+    }
+}
