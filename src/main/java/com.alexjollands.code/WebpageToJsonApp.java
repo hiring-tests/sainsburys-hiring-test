42a6867ca74382639ee4ddaaf2dd675b5634265d
Alex Jollands <alexj.ollands@googlemail.com>
2017-04-21 06:41:08 +0100

    Adding webpage retrieval functionality

--- src/main/java/com.alexjollands.code/WebpageToJsonApp.java
+++ src/main/java/com.alexjollands.code/WebpageToJsonApp.java
@@ -1,8 +1,14 @@
-package java.com.alexjollands.code;
+package com.alexjollands.code;
+
+import com.alexjollands.code.model.Webpage;
 
 public class WebpageToJsonApp{
 
+    private static final String PRODUCT_PAGE_URL = "http://hiring-tests.s3-website-eu-west-1.amazonaws.com/2015_Developer_Scrape/5_products.html";
+
     public static void main(String[] args){
-        System.out.println("Test message and JAR executable");
+        Webpage page = new Webpage(PRODUCT_PAGE_URL);
+        String html = page.getContent();
+        System.out.println("Page content: " + html);
     }
 }
--- src/main/java/com.alexjollands.code/model/Webpage.java
+++ src/main/java/com.alexjollands.code/model/Webpage.java
@@ -1,5 +1,36 @@
-package java.com.alexjollands.code.model;
+package com.alexjollands.code.model;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
 
 public class Webpage {
-    
+
+    private String url;
+    private String content;
+
+
+    public Webpage(String url){
+        this.url = url;
+    }
+
+    public String getUrl(){
+        return url;
+    }
+
+    public String getContent(){
+        if (content == null){
+            try {
+                Document page = Jsoup.connect(this.url).get();
+                this.content = page.outerHtml();
+            }
+            catch (IOException e){
+                // Handle exception logic
+            }
+        }
+        return content;
+    }
 }
